6fd04a7aa705805442b1d23c799862906cc8d325
Chris Purta <cpurta@kochava.com>
2016-09-01 17:05:54 -0700

    Add more functions to Peer.go and remove connneciton
    Remove the connection.go file entirely as it is not really needed or at least
    at this point in time. Added more functions to the peer struct as we need them
    to add, remove, and check other peers that are entering and leaving the
    network.

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
 func main() {
+	peer := NewPeer(1000)
 
+	peer.Start()
 }
--- peer.go
+++ peer.go
@@ -74,6 +74,8 @@ func (p *Peer) Start() {
 
 	log.Printf("Peer [%s] listening on %s:%d", p.PeerID, p.Host, p.Port)
 
+	go p.listenForShutdown()
+
 	for !p.Shutdown {
 		conn, err := l.Accept()
 		if err != nil {
@@ -94,6 +96,48 @@ func (p *Peer) Shutdown() {
 	p.Shutdown = true
 }
 
+func (p *Peer) AddPeer(peerID string) {
+	p.PeerLock.Lock()
+	defer p.PeerLock.Unlock()
+	if _, ok := p.Peers[peerID]; !ok {
+		p.Peers[peerID] = 1
+	} else {
+		log.Printf("Peer [%s] already in list of know peers.\n", peerID)
+	}
+}
+
+func (p *Peer) RemovePeer(peerID string) error {
+	p.PeerLock.Lock()
+	defer p.PeerLock.Unlock()
+	if _, ok := p.Peers[peerID]; ok {
+		delete(p.Peers[peerID])
+	} else {
+		log.Printf("Unable to remove peer [%s] from list of know peers", peerID)
+	}
+}
+
+func (p *Peer) CheckLivePeers() {
+	toDelete := make([]string, 0)
+
+	for host, port := range p.Peers {
+		peerID := fmt.Sprintf("%s:%d", host, port)
+
+		conn, err := net.Dial("tcp", host + ":" + string(port))
+		if err != nil {
+			log.Printf("Error dialing %s: %s", peerID, err.Error())
+		} else {
+			conn.Write(`PING`)
+			status, err := bufio.NewReader(conn).ReadString("\n")
+			if err != nil {
+				log.Printf("Error recieving PING from %s\n", peerID, err.Error())
+				p.RemovePeer(peerID)
+			} else {
+				log.Printf("Successful response from %s\n", peerID)
+			}
+		}
+	}
+}
+
 func handleRequest(conn net.Conn) {
 	buffer := make([]byte, BufferLength)
 
@@ -102,7 +146,21 @@ func handleRequest(conn net.Conn) {
 		log.Printf("Error reading message from connection [%s]: %s\n", conn.RemoteAddr().String(), err.Error())
 	}
 
-	conn.Write([]byte("Message Recieved"))
+	conn.Write([]byte("Message Recieved\n"))
 
 	conn.Close()
+}
+
+func (p *Peer) listenForShutdown() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, unix.SIGTERM)
+	go func() {
+		msg := "Starting shutdown."
+		for s := range sig {
+			log.Printf("Received signal: %v. %s\n", s, msg)
+
+			msg = "Shutdown in progress."
+			p.Shutdown()
+		}
+	}()
 }
